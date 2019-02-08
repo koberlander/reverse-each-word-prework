fbe66db45cc0b8b001cdd579edebb36459a7b116
ko kristin <koberlander115@gmail.com>
2019-02-08 23:47:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,6 +3,11 @@ def reverse_each_word(sentence1)
   sentence1.split(/ /).each {|x| x.reverse >> newSentence} 
 end
 
+new_array = []
+old_array.each do |element|
+  new_array << element.reverse
+end
+
 # def reverse_each_word(sentence2)
 # sentence2.split(/ /)
 # sentence2.collect { |x| x.reverse }
