1f597a07c915e3d6b943a98dd29c7ba30e9f77b2
ko kristin <koberlander115@gmail.com>
2019-02-09 00:19:29 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+def reverse_each_word(sentence1)
+  newSentence = []
+  sentence1.split(/ /).each {|element| newSentence << element.reverse}
+  return newSentence.join(" ")
+end
+
+
+def reverse_each_word(sentence2)
+  sentence2.split(/ /).collect {|element| element.reverse}.join(" ")
+end
